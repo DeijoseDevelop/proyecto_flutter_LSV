92d71bdbc04d6ee6bf2defe77921101b18a41588
Deiver Vazquez <dvazquez@lsv-tech.com>
2022-05-25 07:55:04 -0500

    gallery basic finish

--- lib/src/view/gallery_view.dart
+++ lib/src/view/gallery_view.dart
@@ -11,12 +11,17 @@ class Gallery extends StatelessWidget {
         centerTitle: true,
         titleTextStyle: const TextStyle(fontSize: 30),
       ),
-      body: Column(
+      body: ListView(
+        padding: const EdgeInsets.all(10),
         children: <Widget>[
           _gallery('https://images.pexels.com/photos/417173/pexels-photo-417173.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'),
+          const SizedBox(height: 10),
           _gallery('https://images.pexels.com/photos/36744/agriculture-arable-clouds-countryside.jpg?auto=compress&cs=tinysrgb&h=650&w=940'),
+          const SizedBox(height: 10),
           _gallery('https://images.pexels.com/photos/46710/pexels-photo-46710.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'),
+          const SizedBox(height: 10),
           _gallery('https://images.pexels.com/photos/1181271/pexels-photo-1181271.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'),
+          const SizedBox(height: 10),
           _gallery('https://images.pexels.com/photos/1192057/pexels-photo-1192057.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'),
         ],
       ),
@@ -24,20 +29,37 @@ class Gallery extends StatelessWidget {
   }
 
   Widget _gallery(url) {
-    return Expanded(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          FadeInImage(
-            placeholder: const AssetImage('assets/jar-loading.gif'),
-            image: NetworkImage(url),
-            fadeInDuration: const Duration(milliseconds: 100),
-            height: 400.0,
-            width: 380.0,
-            fit: BoxFit.fitWidth,
+    final card = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        FadeInImage(
+          placeholder: const AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage(url),
+          fadeInDuration: const Duration(milliseconds: 100),
+          height: 250.0,
+          fit: BoxFit.fitWidth,
+        ),
+        const Text('Text to Card'),
+    ],
+    );
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.black45,
+        boxShadow: const <BoxShadow>[
+          BoxShadow(
+            color: Colors.black38,
+            blurRadius: 10,
+            spreadRadius: 2,
+            offset: Offset(2, 2),
           ),
-      ],
-    ),
+        ],
+      ),
+      padding: const EdgeInsets.all(10),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: card,
+      ),
     );
   }
 }
